95349dceb918948448552026c4fc55898e512f67
Nasra <nasraaden@Nasras-MacBook-Pro.local>
2019-12-16 16:29:43 -0800

    did tests for Controls and Display

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,45 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("Open and close gate buttons work", () => {
+    const {getByText, rerender} = render(<Controls closed={false}/>)
+    const openButton = getByText(/close/i);
+    fireEvent.click(openButton);
+    getByText(/close/i);
+
+    rerender(<Controls closed={true}/>)
+    const closeButton = getByText(/open/i);
+    fireEvent.click(closeButton);
+    getByText(/open/i);
+})
+ 
+test("Lock and unlock buttons work", () => {  
+    const {getByText, rerender} = render(<Controls locked={false} />)
+    const unlockButton = getByText(/lock/i);
+    fireEvent.click(unlockButton);
+    getByText(/lock/i);
+
+    rerender(<Controls locked={true}/>)
+    const lockButton = getByText(/unlock/i);
+    fireEvent.click(lockButton);
+    getByText(/unlock/i);
+})
+
+test("Open button is disabled if gate is locked", () => {
+    const toggleClosed= jest.fn();
+    const {getByText} = render(<Controls closed={true} locked={true} toggleClosed={toggleClosed} />)
+    const openDisabled = getByText(/open/i)
+    fireEvent.click(openDisabled);
+    expect(toggleClosed).not.toHaveBeenCalled();
+})
+
+test("Lock button is disabled if gate is open", () => {
+    const toggleLocked = jest.fn();
+    const {getByText} = render(<Controls closed={false} locked={false} toggleLocked={toggleLocked} />)
+    const lockDisabled = getByText(/lock/i)
+    fireEvent.click(lockDisabled);
+    expect(toggleLocked).not.toHaveBeenCalled();
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+test("Locked and unlocked are displayed", () => {
+    const {getByText, rerender} = render(<Display locked={true}/>)
+    getByText(/locked/i);
+
+    rerender(<Display locked={false} />)
+    getByText(/unlocked/i)
+})
+
+test("Open and closed are displayed", () => {
+    const {getByText, rerender} = render(<Display closed={true}/>)
+    getByText(/closed/i);
+
+    rerender(<Display closed={false} />)
+    getByText(/open/i)
+})
+
